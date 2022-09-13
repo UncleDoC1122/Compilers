dac2b75d525df133a5fe266265277790d9cd0af9
Daniil Siyanchev <uncledoctortheone@gmail.com>
2022-09-13 15:31:54 +0400

    Initial commit

---
+++ Compilers.Lab1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Compilers.Lab1;
+
+LexemProcessor processor = new LexemProcessor();
+var result = processor.ProcessFile("./Examples/Example1.txt");
+
+Console.WriteLine("Lexems: ");
+Console.WriteLine("================================================");
+foreach (var lexem in result.Item1)
+{
+	Console.WriteLine(lexem.ToString());
+}
+Console.WriteLine("================================================");
+Console.WriteLine("Variables: ");
+foreach (var variable in result.Item2)
+{
+	Console.WriteLine(variable);
+}
+Console.WriteLine("================================================");
+
+Console.ReadKey();
